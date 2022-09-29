ff32aca14057da7a381e47f716ac5ab2a0f8764f
unknown <shobhitdimri01>
2022-09-29 20:29:42 +0530

    Room reservation added

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -72,7 +72,8 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 	res, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 	if !ok {
-		helpers.ServerError(w, errors.New("can't  get reservation from session"))
+		m.App.Session.Put(r.Context(), "error", "can't get reservation from session")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
 
@@ -83,7 +84,11 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	
+
 	res.Room.RoomName = room.RoomName
+	m.App.InfoLog.Println("ROOM:", res.Room.RoomName)
+	m.App.Session.Put(r.Context(), "reservation", res)
 	sd := res.StartDate.Format("2006-01-02")
 	ed := res.EndDate.Format("2006-01-02")
 
@@ -104,6 +109,11 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 //Post Reservation handles the posting and validation of form
 // PostReservation handles the posting of a reservation form
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		helpers.ServerError(w, errors.New("can't get session"))
+	}
+
 	err := r.ParseForm()
 	if err != nil {
 		m.App.Session.Put(r.Context(), "error", "can't parse form!")
@@ -132,25 +142,32 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	// 	return
 	// }
 
-	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
-	if err != nil {
-		m.App.Session.Put(r.Context(), "error", "invalid data!")
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
+	// roomID, err := strconv.Atoi(r.Form.Get("room_id"))
+	// if err != nil {
+	// 	m.App.Session.Put(r.Context(), "error", "invalid data!")
+	// 	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+	// 	return
+	// }
 
-	reservation := models.Reservation{
-		FirstName: r.Form.Get("first_name"),
-		LastName:  r.Form.Get("last_name"),
-		Phone:     r.Form.Get("phone"),
-		Email:     r.Form.Get("email"),
-		// StartDate: startDate,
-		// EndDate:   endDate,
-		RoomID: roomID,
-	}
+	//Updating my Summary-page
+	reservation.FirstName = r.Form.Get("first_name")
+	reservation.LastName = r.Form.Get("last_name")
+	reservation.Phone = r.Form.Get("phone")
+	reservation.Email = r.Form.Get("email")
+
+	// reservation := models.Reservation{
+	// 	FirstName: r.Form.Get("first_name"),
+	// 	LastName:  r.Form.Get("last_name"),
+	// 	Phone:     r.Form.Get("phone"),
+	// 	Email:     r.Form.Get("email"),
+	// 	// StartDate: startDate,
+	// 	// EndDate:   endDate,
+	// 	RoomID: roomID,
+	// }
 
 	form := forms.New(r.PostForm)
 
+	//minimum requirement specified
 	form.Required("first_name", "last_name", "email")
 	form.Minlength("first_name", 3)
 	form.IsEmail("email")
@@ -171,10 +188,12 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		helpers.ServerError(w, err)
 	}
 
+	m.App.Session.Put(r.Context(), "reservation", reservation)
+
 	restriction := models.RoomRestriction{
-		// StartDate:     startDate,
-		// EndDate:       endDate,
-		RoomID:        roomID,
+		StartDate:     reservation.StartDate,
+		EndDate:       reservation.EndDate,
+		RoomID:        reservation.RoomID,
 		ReservationID: newReservationID,
 		RestrictionID: 1,
 	}
@@ -312,7 +331,6 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 	stringMap := make(map[string]string)
 	stringMap["start_date"] = sd
 	stringMap["end_date"] = ed
-
 	render.Template(w, r, "reservation-summary.html", &models.TemplateData{
 		Data:      data,
 		StringMap: stringMap,
